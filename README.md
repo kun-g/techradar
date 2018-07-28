8a1a3a168364d4ff99c62a25881b455a5318877e
Kun.G <kun.g@qq.com>
2018-07-29 00:12:57 +0800

    adr

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # techradar
 my tech radar
+
+# TODO
+- [ ] 避免点都挤到一起
--- radarData.js
+++ radarData.js
@@ -1,6 +1,6 @@
 var radar_data = [
 	{
-		title: "Kun.G's Technology Radar",
+		title: "Kun.G的个人雷达",
 		stages: [
             { r: 0.35, name:'Adopt',  fill: "rgba(200, 200, 200, 0.8)" },
             { r: 0.60, name:'Trial',  fill: "rgba(200, 200, 200, 0.6)" },
@@ -9,40 +9,52 @@ var radar_data = [
 		],
 		quadrants: [
 			{
-				name: "Techniques",
+				name: "技能",
 				items: [
-					{ name: 'Service Mesh', stage: "Assess" },
-					{ name: "Architecture Decision Records", stage: "Trial", "url": "https://github.com/npryce/adr-tools" },
-					{ name: "Clean Code", stage:'Assess' },
-					{ name: "Zookeeper for App Config", stage:'Assess' },
-					{ name: "Code Reviews", stage: "Trial" },
-					{ name: "Dependency Injection", stage: "Assess" },
+					{ name: '技能管理', stage: "Assess" },
+					{ name: '写作', stage: "Assess" },
+					{ name: '读书', stage: "Assess" },
+					{ name: '阅读', stage: "Assess" },
 				]
 			},
 			{
-				name: "Tools",
+				name: "工具",
 				items: [
-					{ name: 'Hubot', stage: "Adopt" },
+					{ name: '技术雷达', stage: "Trial" },
 					{ name: 'Docker', stage: "Trial" },
+					{ name: 'Check List', stage: "Assess" },
+					{ name: '卡诺图', stage: "Assess" },
+					{ name: 'Hubot', stage: "Adopt" },
 					{ name: 'mongoDB', stage: "Assess" },
 					{ name: 'Ansible', stage: "Assess" },
 					{ name: 'Hip Chat', stage: "Assess" },
 					{ name: 'Trello', stage: "Assess" },
 					{ name: 'ELK', stage: "Assess" },
 					{ name: 'haproxy', stage: "Assess" },
-				]
-			},
-			{
-				name: "Platforms",
-				items: [
+					{ name: 'home assistant', stage: "Assess" },
 					{ name: 'Kubernetes', stage: "Assess" },
 				]
 			},
 			{
-				name: "Languages & Frameworks",
+				name: "思想",
+				items: [
+					{ name: '边缘思维', stage: "Assess" },
+					{ name: '能力圈', stage: "Assess" },
+				]
+			},
+			{
+				name: "技术",
 				"items": [
+					{ name: 'Service Mesh', stage: "Assess" },
+					{ name: "Architecture Decision Records", stage: "Trial", "url": "https://github.com/npryce/adr-tools" },
+					{ name: "Clean Code", stage:'Assess' },
+					{ name: "Zookeeper for App Config", stage:'Assess' },
+					{ name: "Code Reviews", stage: "Trial" },
+					{ name: "Dependency Injection", stage: "Assess" },
 					{"name":"Swift", stage: "Assess" },
 					{"name":"C", stage: "Assess" },
+					{ name: '树梅派', stage: "Assess"  },
+					{ name: 'Arduino', stage: "Assess"  },
 				]
 			}
 		]
@@ -82,42 +94,7 @@ var radar_data = [
 			}
 		]
 	},
-	{
-		title: "Kun.G的个人雷达",
-		stages: [
-			{r:0.35, name:'Adopt',  fill: "rgba(211, 224, 16, 0.5)"},
-			{r:0.60, name:'Trial',  fill: "rgba(211, 224, 16, 0.5)"},
-			{r:0.80, name:'Assess', fill: "rgba(211, 224, 16, 0.5)"},
-			{r:1.00, name:'Hold',   fill: "rgba(211, 224, 16, 0.5)"}
-		],
-		quadrants: [
-			{
-				name: "工具",
-				items: [
-					{ name: '技术雷达', stage: "Trial" },
-					{ name: 'Check List', stage: "Assess" },
-				]
-			},
-			{
-				name: "思想",
-				items: [
-					{ name: '边缘思维', stage: "Assess" },
-				]
-			},
-			{
-				name: "平台",
-				items: [
-					{ name: '树梅派', stage: "Assess"  },
-					{ name: 'Arduino', stage: "Assess"  },
-				]
-			},
-			{
-				name: "其它",
-				"items": [
-				]
-			}
-		]
-	}
+	
 ]
 
 // 不这样做，fill的变化无法被vue监听到
